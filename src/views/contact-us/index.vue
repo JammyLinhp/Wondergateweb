e557167e4613094453a92a6cd1609c9a1f80131e
feng.xie <feng.xie@onerway.com>
2024-06-14 17:20:05 +0800

    ssr 完成 补充

--- src/views/contact-us/index.vue
+++ src/views/contact-us/index.vue
@@ -13,10 +13,9 @@
         ></div>
         <a-tabs
           class="layout-content-subspacing-small app-color-text-main app-description-text-small app-detail-font app-text-description-title"
-          v-if="informationList"
         >
           <a-tab-pane :key="index" v-for="(item, index) in informationList">
-            <template #tab>{{ item.title }} </template>
+            <template #tab>{{ item.title }}</template>
             <a-row :gutter="36">
               <a-col :span="9">
                 <div>{{ $t('moo.contact.email') }} :</div>
@@ -80,29 +79,31 @@ const informationList = ref<ICard[]>([
     title: 'Hong Kong',
     name: 'Wondergate Co., Limited',
     description:
-      'FLAT/RM 121 1/F LIVEN HOUSE NOS.61-63 KING YIP STREET KWUN TONG',
+      'FLAT/RM 121 1/F LIVEN HOUSE NOS.61-63 KING YIP STREET KWUN TONG'
   },
   {
     title: 'Canada',
     name: 'WONDERGATE FINTECH INC.',
-    description: '44322 YALE RD UNIT 3B #198 CHILLIWACK BC V2R 4H1 CANADA',
+    description: '44322 YALE RD UNIT 3B #198 CHILLIWACK BC V2R 4H1 CANADA'
   },
   {
     title: 'America',
     name: 'Wondergate Limited',
-    description: '1401 21ST ST STE R  SACRAMENTO, CA 95811',
-  },
+    description: '1401 21ST ST STE R  SACRAMENTO, CA 95811'
+  }
 ]);
 </script>
 
 <style lang="less">
 @import '@/styles/base';
+
 .ant-tabs-top > .ant-tabs-nav::before,
 .ant-tabs-bottom > .ant-tabs-nav::before,
 .ant-tabs-top > div > .ant-tabs-nav::before,
 .ant-tabs-bottom > div > .ant-tabs-nav::before {
   border: 0px;
 }
+
 .contact-description {
   padding: 1.65rem 0 0.8rem;
 }
